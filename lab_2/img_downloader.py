1d153705e61f34ee5df5e69906eb584b71fd6f72
Victor Popov <119051119+Vantany@users.noreply.github.com>
2024-10-29 21:04:00 +0400

    image downloader init

---
+++ lab_2/img_downloader.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+from icrawler.builtin import GoogleImageCrawler
+
+
+def img_downloader(keyword:str , save_path:str)->None:
+    """
+    Функция для загрузки изображений по ключевому слову
+    :param keyword: ключевое слово для поиска
+    :param save_path: путь к папке для сохранениия
+    :return: None
+    """
+
+    try:
+        google_crawler = GoogleImageCrawler(storage={'root_dir': save_path})
+        google_crawler.crawl(keyword=keyword, max_num=75)
+    except:
+        raise Exception("Downloading went wrong")
--- lab_2/main.py
+++ lab_2/main.py
@@ -1,5 +1,7 @@
 import argparse
 
+from img_downloader import img_downloader
+
 def get_arguments() -> tuple:
     """
     Функция для получения аргуметов
@@ -18,7 +20,11 @@ def get_arguments() -> tuple:
     
     return parser.parse_args().keyword, parser.parse_args().sp, \
             parser.parse_args().ap
-    
+
+ 
 if __name__ == "__main__":
-    keyword, sp, ap = get_arguments()
-    
+    keyword, save_path, annotation_path = get_arguments()
+    try:
+        img_downloader(keyword=keyword, save_path=save_path)
+    except:
+        raise Exception("Something went wrong")
